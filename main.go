fb49a622c785cbc608fe38021af164be53e326a5
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-14 08:08:38 +1000

    Update main.go
    - implement `cleanInput`

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	var cleanedInput []string
+	text = strings.TrimSpace(text)
+	for word := range strings.SplitSeq(text, " ") {
+		word = strings.TrimSpace(word)
+		if len(word) > 0 {
+			cleanedInput = append(cleanedInput, strings.ToLower(word))
+		}
+	}
+	return cleanedInput
 }
