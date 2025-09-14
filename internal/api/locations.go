3f88f3f25a0f2bfa6e3ec37b2190c5bca7278485
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-15 09:50:26 +1000

    Update internal/api/locations.go
    - add explore command function

--- internal/api/locations.go
+++ internal/api/locations.go
@@ -23,6 +23,80 @@ type locationResults struct {
 	Results  []locationEndpoint `json:"results"`
 }
 
+type encounterMethod struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type version struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type encounterVersionDetails struct {
+	Rate    int     `json:"rate"`
+	Version version `json:"version"`
+}
+
+type encounterMethodRates struct {
+	EncounterMethod encounterMethod           `json:"encounter_method"`
+	VersionDetails  []encounterVersionDetails `json:"version_details"`
+}
+
+type location struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type language struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type names struct {
+	Language language `json:"language"`
+	Name     string   `json:"name"`
+}
+
+type pokemon struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type method struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type encounterDetails struct {
+	Chance          int    `json:"chance"`
+	ConditionValues []any  `json:"condition_values"`
+	MaxLevel        int    `json:"max_level"`
+	Method          method `json:"method"`
+	MinLevel        int    `json:"min_level"`
+}
+
+type pokemonEncounterVersionDetails struct {
+	EncounterDetails []encounterDetails `json:"encounter_details"`
+	MaxChance        int                `json:"max_chance"`
+	Version          version            `json:"version"`
+}
+
+type pokemonEncounters struct {
+	Pokemon        pokemon                          `json:"pokemon"`
+	VersionDetails []pokemonEncounterVersionDetails `json:"version_details"`
+}
+
+type locationAreaResults struct {
+	EncounterMethodRates []encounterMethodRates `json:"encounter_method_rates"`
+	GameIndex            int                    `json:"game_index"`
+	ID                   int                    `json:"id"`
+	Location             location               `json:"location"`
+	Name                 string                 `json:"name"`
+	Names                []names                `json:"names"`
+	PokemonEncounters    []pokemonEncounters    `json:"pokemon_encounters"`
+}
+
 func GetLocationResults(url string, cache *pokecache.Cache) (locationResults, error) {
 	var results locationResults
 	res, _ := cache.Get(url)
@@ -68,6 +142,19 @@ func GetPreviousLocations(c *LocationConfig, cache *pokecache.Cache) ([]string, 
 	}
 	for _, r := range res.Results {
 		result = append(result, r.Name)
+	}
+	return result, nil
+}
+
+func GetPokemonFromArea(area string, cache *pokecache.Cache) ([]string, error) {
+	var result []string
+	var results locationAreaResults
+	res, _ := cache.Get("https://pokeapi.co/api/v2/location-area/" + area)
+	if err := json.Unmarshal(res, &results); err != nil {
+		return result, nil
+	}
+	for _, pokemon := range results.PokemonEncounters {
+		result = append(result, pokemon.Pokemon.Name)
 	}
 	return result, nil
 }
